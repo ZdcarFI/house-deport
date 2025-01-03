e5f68a2ed45949662cb977dbe3c1983b45a8e27d
Kevin Jhosep <kevinjhosepct@gmail.com>
2025-01-02 19:14:46 -0500

    update config environment

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
     APP_NAME: process.env.NEXT_PUBLIC_APP_NAME || 'MyApp',
-    JWT_SECRET: process.env.JWT_SECRET || 'secret',
-    API: process.env.API || 'http://localhost:8000',
+    JWT_SECRET: process.env.JWT_SECRET || 'exampled_secret',
+    API: process.env.API || 'https://innocent-busy-aardvark.ngrok-free.app',
 };
