7dcbb831de4fd43a897cba737b80f65fb0a14efd
ZdcarFI <121211637+ZdcarFI@users.noreply.github.com>
2025-02-07 05:04:29 -0500

    unautorized

--- src/app/unauthorized/page.tsx
+++
@@ -1,9 +0,0 @@
-const Unauthorized = () => {
-    return (
-        <div>
-            <h1>No tienes permiso para acceder a esta página</h1>
-        </div>
-    );
-};
-
-export default Unauthorized;
