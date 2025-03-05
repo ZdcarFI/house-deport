d5f6e7483895140e23bfa7fc6003eeb0d7d5d1c0
Kevin Jhosep <kevinjhosepct@gmail.com>
2025-03-05 13:52:55 -0500

    feat add limit max to pieChart

--- src/components/dashboard/ProductosVentas.tsx
+++ src/components/dashboard/ProductosVentas.tsx
@@ -10,6 +10,7 @@ export const ProductSalesPercentage: React.FC = () => {
   const productSales = useMemo(() => {
     const sales: { [key: string]: number } = {};
     let totalSales = 0;
+
     orders.forEach(order => {
       order.details.forEach(detail => {
         const productName = detail.product.name;
@@ -17,37 +18,39 @@ export const ProductSalesPercentage: React.FC = () => {
         totalSales += detail.total;
       });
     });
-    return Object.entries(sales).map(([name, value]) => ({
-      name,
-      value,
-      percentage: ((value / totalSales) * 100).toFixed(2),
-    }));
+
+    return Object.entries(sales)
+        .map(([name, value]) => ({
+          name,
+          value,
+          percentage: ((value / totalSales) * 100).toFixed(2),
+        }))
+        .sort((a, b) => b.value - a.value) // Ordenar de mayor a menor
+        .slice(0, 10); // Tomar solo los 10 primeros
   }, [orders]);
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8'];
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8', '#A28DFF', '#FF6680', '#66CCFF', '#FF99CC', '#99FF99'];
 
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <PieChart>
-        <Pie
-          data={productSales}
-          cx="50%"
-          cy="50%"
-          labelLine={false}
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
-          max={10}
-          label={({ name, percentage }) => `${name}: ${percentage}%`}
-        >
-          {productSales.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-        <Tooltip />
-        <Legend />
-      </PieChart>
-    </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart>
+          <Pie
+              data={productSales}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+              label={({ name, percentage }) => `${name}: ${percentage}%`}
+          >
+            {productSales.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
   );
 };
-
